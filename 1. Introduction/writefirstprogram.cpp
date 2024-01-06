149a16a00af46ced3c6aed722236543bf1a042fb
Aditi Rastogi <138372747+aditiras@users.noreply.github.com>
2024-01-06 23:15:25 +0530

    First Program Added
    Written first program of C++ language with basic concepts

---
+++ 1. Introduction/writefirstprogram.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 50;
+    // cout is used for output
+    cout << num << endl;
+
+    // to get the size in bytes
+    cout << sizeof(num) << endl;
+
+    int marks;
+    // cin is used tu take input
+    cin >> marks;
+    cout << "Marks entered is " << marks << endl;
+}
